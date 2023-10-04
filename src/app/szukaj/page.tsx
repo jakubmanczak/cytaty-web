946bdd326aeb8f16fa239a296fc79f9e3e2b642e
jakubmanczak <jakub@manczak.net>
2023-10-04 21:03:48 +0200

    default search screen with 'no quotes' msg

--- src/app/szukaj/page.tsx
+++ src/app/szukaj/page.tsx
@@ -1,13 +1,46 @@
 import { Navigation } from "@/components/Navigation";
+import Image from "next/image";
 
 export default function Search() {
   return (
     <>
       <Navigation />
-      <div className="max-w-4xl mx-auto py-4">
-        <h1 className="font-bold text-xl">Szukaj</h1>
-        <p>Strona w konstrukcji.</p>
-      </div>
+      <main className="max-w-4xl mx-auto py-4">
+        {/* SEARCH SECTION */}
+        <section className="flex flex-col gap-3 my-6">
+          <div className="w-full flex flex-row shadow rounded">
+            <input
+              type="text"
+              placeholder={`Szukaj w cytatach, kontekstach lub autorach...`}
+              className="w-full p-3 rounded rounded-tr-none rounded-br-none"
+            />
+            <button
+              className="
+              flex justify-center items-center w-12
+              bg-gradient-to-t from-violet-700 to-blue-600
+              rounded rounded-tl-none rounded-bl-none drop-shadow
+            "
+            >
+              <Image
+                src="/search-white.svg"
+                alt="ikona szukania reprezentująca przycisk do zatwierdzenia wyszukania"
+                width={24}
+                height={24}
+              />
+            </button>
+          </div>
+          {/* <div className="w-full flex flex-row">
+            <p>aa</p>
+          </div> */}
+        </section>
+        <hr className="border-slate-300" />
+        {/* RESULTS SECTION */}
+        <section>
+          <h1 className="text-2xl font-bold text-slate-500 text-center mt-8">
+            Nie znaleziono cytatów.
+          </h1>
+        </section>
+      </main>
     </>
   );
 }
